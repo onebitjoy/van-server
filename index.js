919b26b5f8e3895df19752a5fa063077bda8b9f9
abhishek <95994085+onebitjoy@users.noreply.github.com>
2024-02-27 09:50:28 +0530

    Trust Proxy enabled

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const limiter = rateLimit({
 
 app.use(limiter)
 app.use(compression())
+app.enable('trust proxy')
 
 const data = [
   { id: "1", name: "Modest Explorer", price: 60, description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", type: "simple", hostId: "123" },
